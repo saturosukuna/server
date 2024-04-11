9cb1e06a4f6dea9990617a2cca8dabb5c88464ae
saturosukuna <157040743+saturosukuna@users.noreply.github.com>
2024-04-11 14:49:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/rajesh', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB', err));
 
